060f0f79f3f508f9a2d337920e72d0e167caf94d
Uğur <ugurkk2599@gmail.com>
2022-09-19 11:07:04 +0300

    Delete rust_blockchain directory

--- rust_blockchain/target/debug/deps/blockchain.d
+++
@@ -1,5 +0,0 @@
-C:\Users\ugurk\Desktop\SolanaRustBootcamp\Assignments\rust_blockchain\target\debug\deps\blockchain.exe: src/main.rs
-
-C:\Users\ugurk\Desktop\SolanaRustBootcamp\Assignments\rust_blockchain\target\debug\deps\blockchain.d: src/main.rs
-
-src/main.rs:
